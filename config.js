9ec15b72ceaf8126a085d5d2bd4b8b7750cf2441
MirZa <152170963+theycallmeprofessor@users.noreply.github.com>
2024-11-05 01:07:59 -0800

    Update config.js

--- config.js
+++ config.js
@@ -2,8 +2,8 @@
 
 
 require("./all/module")
-global.owner = "254742491666" 
-global.namaCreator = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.owner = "923168066637" 
+global.namaCreator = "MIRZ4" 
 global.autoJoin = false 
 global.antilink = false 
 global.versisc = '6.0' 
@@ -12,7 +12,7 @@ global.isLink = 'https://whatsapp.com/channel/0029VaaqaSp0LKZDuwe5SI3e'
 global.sessionName = 'session'
 global.thumb = fs.readFileSync("./thumb.png") 
 global.audionya = fs.readFileSync("./all/sound.mp3") 
-global.packname = "𝕶𝖎𝖓𝖌 𝕾𝖆𝖒" 
+global.packname = "𝕶𝖎𝖓𝖌 MIRZ4" 
 global.author = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
 global.jumlah = "5" 
 global.namabot = "𝐒𝚸𝚰𝐃𝚵𝚪-𝛁6" 
